05ff3e9f56ac1aa65cc6d371c1f98ac1b22f47a7
anthonybuncio <anthonyb.ft@gmail.com>
2023-04-26 18:49:32 -0500

    Changes to deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_GTM_ANALYTICS);
   return (
     <>
       <section className="bg-gray-900 text-white">
